3568ac58599e563118838413e5b563bbab366066
Kevan24 <149030240+Kevan24@users.noreply.github.com>
2024-01-25 08:57:27 +0700

    Add files via upload

---
+++ Pertemuan 3/operator_penugasan.py
@@ -0,0 +1,41 @@
+# Penjumlahan
+print('Penjumlahan')
+a = 5
+print(a)
+a += 3
+print(a)
+
+# Pengurangan 
+print('Pengurangan')
+b = 18
+print(b)
+b -= 6
+print(b)
+
+# Perkalian
+print('Perkalian')
+c = 3
+print(c)
+c *= 2
+print(c)
+
+# Pembagian
+print('Pembagian')
+d = 14
+print(d)
+d /= 3
+print(d)
+
+# Perpangkatan
+print('Perpangkatan')
+e = 2
+print(e)
+e **= 15
+print(e)
+
+# Modulus
+print('Modulus')
+f = 18
+print(f)
+f %= 5
+print(f)
---
+++ Pertemuan 3/operator_perbandingan.py
@@ -0,0 +1,15 @@
+x = 5
+y = 3
+
+# lebih dari
+print(x > y)
+# kurang dari
+print(x < y)
+# lebih dari atau sama dengan
+print(x >= y)
+#kurang dari atau sama dengan
+print(x <= y)
+# sama dengan
+print(x == y)
+# tidak sama dengan
+print(x != y)
